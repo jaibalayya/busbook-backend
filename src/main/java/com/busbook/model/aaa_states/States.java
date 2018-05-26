ec0604fe8c175061795524b0ff357c9c1e44cd7e
DESKTOP-A9NEL06\Asish Pradhan <apradhan121@gmail.com>
2018-05-26 15:12:15 +0530

    all attributes, accessors and eqals(), hashCode() added

--- src/main/java/com/busbook/model/aaa_states/States.java
+++ src/main/java/com/busbook/model/aaa_states/States.java
@@ -1,9 +1,67 @@
 package com.busbook.model.aaa_states;
 
-public class States {
+import java.io.Serializable;
 
-	public States() {
-		// TODO Auto-generated constructor stub
+public class States implements Serializable {
+/*	aaa_state_id (PK)
+	aaa_state_name
+	aaa_status
+	*/
+	protected String stateId;
+	protected String stateName;
+	protected String status;
+	public String getStateId() {
+		return stateId;
+	}
+	public void setStateId(String stateId) {
+		this.stateId = stateId;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((stateId == null) ? 0 : stateId.hashCode());
+		result = prime * result + ((stateName == null) ? 0 : stateName.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		States other = (States) obj;
+		if (stateId == null) {
+			if (other.stateId != null)
+				return false;
+		} else if (!stateId.equals(other.stateId))
+			return false;
+		if (stateName == null) {
+			if (other.stateName != null)
+				return false;
+		} else if (!stateName.equals(other.stateName))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
 	}
 
 }
